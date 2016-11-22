53554c45ec2efde1505a2c92bd79ebc21f8b1dab
Tuannv1 <tuannv1@peacesoft.net>
2016-11-22 13:58:02 +0700

    ok

---
+++ frontend/mobileapp/app/Home/dhgate-box/Model/dhgate-box.ts
@@ -0,0 +1,10 @@
+/* * * ./app/home/dhgate-box/model/dhgate-box.ts * * */
+export class DhgateBox {
+    constructor(
+        public id: string,
+        public src: string, 
+        public title:string,
+        public price: string,
+        public currency: string
+        ){}
+}

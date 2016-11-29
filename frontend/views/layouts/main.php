9dad31056b673291411d594832f942ccec6b8b9b
Tuannv1 <tuannv1@peacesoft.net>
2016-11-29 15:32:01 +0700

    ok

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -81,32 +81,17 @@
         <!-- Polyfill(s) for older browsers -->
         <script src="node_modules/core-js/client/shim.min.js"></script>
 
-<<<<<<< HEAD
-    <script src="node_modules/zone.js/dist/zone.js"></script>
-    <script src="node_modules/reflect-metadata/Reflect.js"></script>
-    <script src="node_modules/systemjs/dist/system.src.js"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-    <script src="systemjs.config.js"></script>
-    <script>
-      System.import('app').catch(function(err){ console.error(err); });
-    </script>
+		<script src="node_modules/zone.js/dist/zone.js"></script>
+		<script src="node_modules/reflect-metadata/Reflect.js"></script>
+		<script src="node_modules/systemjs/dist/system.src.js"></script>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+		<script src="systemjs.config.js"></script>
+		<script>
+		  System.import('app').catch(function(err){ console.error(err); });
+		</script>
   </head>
   <body>
-        <script src="node_modules/zone.js/dist/zone.js"></script>
-        <script src="node_modules/reflect-metadata/Reflect.js"></script>
-        <script src="node_modules/systemjs/dist/system.src.js"></script>
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-        <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
-
-        <script src="systemjs.config.js"></script>
-        <script>
-            System.import('app').catch(function (err) {
-                console.error(err);
-            });
-        </script>
-    </head>
-    <body>
     <mobile-app>Loading...</mobile-app>
-    </body>
-    </html>
+  </body>
+  </html>
 <?php endif; ?>

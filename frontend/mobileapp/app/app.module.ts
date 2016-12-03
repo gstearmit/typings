ce97ecf9480337e60c798af286448c70edb4db42
Tuannv1 <tuannv1@peacesoft.net>
2016-12-03 09:52:09 +0700

    ok

--- frontend/mobileapp/app/app.module.ts
+++ frontend/mobileapp/app/app.module.ts
@@ -2,37 +2,18 @@ import { NgModule }       from '@angular/core';
 //import { BrowserModule }  from '@angular/platform-browser';
 //import { FormsModule }    from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
-<<<<<<< HEAD
+
 import { HomeModule } from './home/home.module';
 
 import { AppComponent }       from './app.component';
 import { NotFoundComponent } from './404/404.component';
 import { AppRoutingModule } from './app.routes'; 
-=======
-
-import { HomeModule } from './home/home.module';
-import { AmazonModule } from './amazon/amazon.module';
-import { EbayModule } from './ebay/ebay.module';
-
-import { LeftMenuComponent } from './header/left-menu.component';
-
-import { AppComponent }       from './app.component';
-import { SliderService } from './slider/weshop/services/slider.service';
-import { HotdealService } from './component-common/hot-deal/services/hotdeal.service';
-import { AmazonBoxService } from './amazon/amazon-box/services/amazon-box.service';
-import { EbayBoxService } from './ebay/ebay-box/services/ebay-box.service';
-import { DhgateBoxService } from './dhgate/dhgate-box/services/dhgate-box.service';
-import { RecentlyviewService } from './component-common/recently-view/services/recentlyview.service';
-import { routing } from './app.routes';
-  
->>>>>>> 5b52b9bbb34dfbb23bd6c5d91ac5f7805e5b41e1
 @NgModule({
   imports: [
     // BrowserModule,
     // FormsModule,
     HttpModule,
     JsonpModule,
-<<<<<<< HEAD
 	HomeModule,
     AppRoutingModule
   ],
@@ -41,24 +22,6 @@ import { routing } from './app.routes';
 	NotFoundComponent
   ],
   providers: [
-=======
-	EbayModule,
-    AmazonModule,
-	HomeModule,
-    routing
-  ],
-  declarations: [
-    AppComponent,
-    LeftMenuComponent
-  ],
-  providers: [
-    SliderService,
-    HotdealService,
-    AmazonBoxService,
-    DhgateBoxService,
-    EbayBoxService,
-    RecentlyviewService,
->>>>>>> 5b52b9bbb34dfbb23bd6c5d91ac5f7805e5b41e1
   ],
   bootstrap: [ AppComponent ]
 })
--- frontend/mobileapp/app/ebay/ebay.routes.ts
+++ frontend/mobileapp/app/ebay/ebay.routes.ts
@@ -2,29 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { EbayComponent }   from './portal/ebay.component';
-<<<<<<< HEAD
+import { EbayBrowserComponent } from "./browser/browser.component";
+import {EbayDetailComponent} from "./detail/detail.component";
 
 const routes: Routes = [
-  { path: '', component: EbayComponent },
+  { path: 'ebay', component: EbayComponent },
+  { path: 'ebay/browser', component: EbayBrowserComponent },
+  { path: 'ebay/detail', component: EbayDetailComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
-=======
-import { EbayBrowserComponent } from "./browser/browser.component";
-import {EbayDetailComponent} from "./detail/detail.component";
-
-@NgModule({
-  imports: [
-    RouterModule.forChild([
-      { path: 'ebay', component: EbayComponent },
-      { path: 'ebay/browser', component: EbayBrowserComponent },
-      { path: 'ebay/detail', component: EbayDetailComponent },
-    // { path: 'amazon-home/portal', component: AmazonPortalComponent }
-    ])
-  ],
-  exports: [RouterModule]
->>>>>>> 5b52b9bbb34dfbb23bd6c5d91ac5f7805e5b41e1
 })
 export class EbayRoutingModule { }
+

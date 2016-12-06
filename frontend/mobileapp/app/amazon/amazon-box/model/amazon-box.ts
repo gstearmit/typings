591d05049384950e956ab9c1f8991c0acc8bc84e
Quan NKT <quannkt@peacesoft.net>
2016-12-06 11:01:54 +0700

    asdasda

--- frontend/mobileapp/app/amazon/amazon-box/model/amazon-box.ts
+++ frontend/mobileapp/app/amazon/amazon-box/model/amazon-box.ts
@@ -1,7 +1,9 @@
 /* * * ./app/home/amaxon-box/model/amazon-box.ts * * */
 export class AmazonBox {
     constructor(
-        public listimage: {},
-        public slider: {}
+        public status: number,
+        public success:number,
+        public messages:string,
+        public data:{'uploadUrl':string,'cms_block_data_image_banner':{}}
         ){}
 }
--- frontend/mobileapp/app/component-common/hot-deal/services/hotdeal.service.ts
+++ frontend/mobileapp/app/component-common/hot-deal/services/hotdeal.service.ts
@@ -14,13 +14,13 @@ export class HotdealService {
      // Resolve HTTP using the constructor
      constructor (private http: Http) {}
      // private instance variable to hold base url
-    private UrlApi = 'http://192.168.11.252:88/';
-     
+    private UrlApi = 'http://192.168.11.252:88/block/getblock';
+
      // Fetch all existing comments
      getDataHotdeal() : Observable<Hotdeal[]>{
-         return this.http.get(this.UrlApi+'?pageId=34&blockName=category-product')     
+         let headers = new Headers({ 'Accept': 'application/json' }); // ... Set content type to JSON
+         return this.http.get(this.UrlApi+'?pageId=34&blockName=category-product',{headers: headers})
                          .map((res:Response) => res.json())
                          .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
-        
      }
 }

c4657093eb0a6c5b00d56a96e9243940e3a2ac3b
Quan NKT <quannkt@peacesoft.net>
2016-12-06 08:40:58 +0700

    asd

--- frontend/mobileapp/app/app.module.ts
+++ frontend/mobileapp/app/app.module.ts
@@ -8,20 +8,7 @@ import { HomeModule } from './home/home.module';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './404/404.component';
 import { AppRoutingModule } from './app.routes';
-<<<<<<< HEAD
 import { TranslateModule } from 'ng2-translate';
-=======
-import { TranslateModule, TranslatePipe } from 'ng2-translate';
-import { ShareModule} from './share/share.module';
-
-
-import { SliderService } from './slider/weshop/services/slider.service';
-import { HotdealService } from './component-common/hot-deal/services/hotdeal.service';
-import { AmazonBoxService } from './amazon/amazon-box/services/amazon-box.service';
-import { EbayBoxService } from './ebay/ebay-box/services/ebay-box.service';
-import { DhgateBoxService } from './dhgate/dhgate-box/services/dhgate-box.service';
-import { RecentlyviewService } from './component-common/recently-view/services/recentlyview.service';
->>>>>>> f182dd954ea052272b0e6f881fa65ac72a8e0421
 
 @NgModule({
   imports: [

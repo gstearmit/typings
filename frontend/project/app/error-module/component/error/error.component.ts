f8f879a6999b3e57e74fe736b0086a948faac4d7
Tuannv1 <tuannv1@peacesoft.net>
2016-12-08 09:53:51 +0700

    ok

---
+++ frontend/project/app/error-module/component/error/error.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'mobile-app',
+    templateUrl: 'error.component.html',
+    providers: [
+        // TranslateService
+    ],
+})
+export class ErrorComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() {
+
+    }
+}
